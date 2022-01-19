ffcd1d1143a1d04a9b711c0279ecdc158a23cbb9
mhlee21 <mihyun0921@gmail.com>
2022-01-19 21:25:24 +0900

    0119 TIL
    * 0119_함수.md
    * 0119_모듈.md
    * calcpkg/, main.py : 패키지 실습

---
+++ 01_python/220119/main.py
@@ -0,0 +1,13 @@
+# import calcpkg.operation
+# import calcpkg.geometry
+
+# print(calcpkg.operation.add(10, 20))
+# print(calcpkg.operation.mul(10, 20))
+
+# print(calcpkg.geometry.triangle_area(30, 40))    # geometry 모듈의 triangle_area 함수 사용
+# print(calcpkg.geometry.rectangle_area(30, 40))   # geometry 모듈의 rectangle_area 함수 사용
+
+from re import M
+from calcpkg.operation import *
+print(add (10, 20))
+print(mul (10, 20))
